68523f27018ddbb79d8d41c23511b581c37a22bf
Sohaib-1704 <33494217+Sohaib-1704@users.noreply.github.com>
2020-02-15 14:08:41 +0530

    commit

--- Login/src/main/java/com/cp/onlinewallet/ui/MainClass.java
+++ Login/src/main/java/com/cp/onlinewallet/ui/MainClass.java
@@ -3,8 +3,16 @@ package com.cp.onlinewallet.ui;
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int choice=3;
+		System.out.println("\n1. Login \n2. SignUp");
+		switch(choice) {
+		case 1: 
+			System.out.println("login");
+			break;
+		case 2:
+			System.out.println("Signup");
+		}
+		
 	}
 
 }
